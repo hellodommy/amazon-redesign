d8404745dd7fa0138775046097362ca14fe9b59a
Dominique Ng <dominique@u.nus.edu>
2020-09-30 23:43:58 +0800

    Remove unused JS

--- js/script.js
+++ js/script.js
@@ -6,15 +6,3 @@ $(document).ready(function() {
     });
 
 })
-
-/*
-    $(function() {                       //run when the DOM is ready
-        $(".clickable").click(function() {  //use a class, since your ID gets mangled
-            $(this).addClass("active");      //add the class to the clicked element
-    });
-
-        $(".tab-option a").on("click", function (e) {
-      $(e.currentTarget).addClass("active");
-    });
-
-*/
